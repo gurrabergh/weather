8c07fc3f7f06d6ff3c86ca3efa0d4b5e44d5f2ad
gurrabergh <gurrabe@hotmail.com>
2020-12-03 15:35:34 +0000

    debugging

--- config/weather.php
+++ config/weather.php
@@ -1,24 +1,6 @@
 <?php
-/**
- * Configuration file for request service.
- */
+
+
 return [
-    // Services to add to the container.
-    "services" => [
-        "weather" => [
-            "shared" => true,
-            "active" => true,
-            "callback" => function () {
-                $obj = new \Anax\Weather\Weather();
-
-                $cfg = $this->get("configuration");
-
-                $config = $cfg->load("weather.php");
-
-                $obj->setApiKey($config);
-
-                return $obj;
-            }
-        ],
-    ],
+    "key" => file_get_contents(ANAX_INSTALL_PATH."/data/PRIVATE_TOKEN")
 ];
